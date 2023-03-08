57616d550d32ca5ac5e71733fb2f4290d6737c7a
freebee-salute <99798960+freebee-salute@users.noreply.github.com>
2023-03-08 15:45:20 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,30 @@
+package org.example;
+
+import static java.util.Arrays.*;
+
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
-	static String[][] array = new String[5][2];
 
+	static String[][] array = {
+			{"11020", "viel Sonne"},
+			{"12020", "nicht so viel Sonne"},
+			{"11420", "viel Sonne"},
+			{"11520", "keine Sonne"},
+			{"11020", "sehr viel Sonne"},
+			{"13020", "wenig Sonne"},
+			{"61020", "kaum Sonne"},
+			{"98020", "enorm viel Sonne"}
+	};
 	public static void main(String[] args) {
-		array[0][0] = "11020";
-		array[1][0] = "12002";
-		array[2][0] = "13003";
-		array[3][0] = "14004";
-		array[4][0] = "15005";
-		array[0][1] = "viel Sonne";
-		array[1][1] = "nicht so viel sonne";
-		array[3][1] = "so viel sonne";
-		array[4][1] = "kaum sonne";
-
 		Scanner scan = new Scanner(System.in);
 		System.out.println("PLZ eingeben: ");
 		int input = scan.nextInt();
 
-		while (input != 0) {
+		while(input != 0) {
 			System.out.println("Sonne? " + holeSonnenStunden(input));
 			System.out.println("PLZ eingeben: ");
 			input = scan.nextInt();
@@ -26,23 +32,25 @@ public class Main {
 	}
 
 	private static String holeSonnenStunden(int input) {
-		if (input < Integer.parseInt(array[0][0]))
-			return Integer.toString(-1);
 
-		int pos = 0;
-		boolean gefunden = false;
-		while (!gefunden && pos < array.length - 1) {
-			if (Integer.parseInt(array[pos][0]) == input) {
-				gefunden = true;
-			} else {
-				pos++;
-			}
-		}
-		if (!gefunden) {
+		int firstValue = Integer.parseInt(array[0][0]);
+		int lastValue = Integer.parseInt(array[array.length-1][0]);
+		while(input > lastValue) {
 			input--;
-			holeSonnenStunden(input);
+		}
+		if(input < firstValue) {
+			return Integer.toString(-1);
 		}
 
-		return array[pos][1];
+		int finalInput = input;
+		Optional<String> result = IntStream.range(0, array.length)
+				.filter(i -> Integer.parseInt(array[i][0]) == finalInput)
+				.mapToObj(i -> array[i][1])
+				.findFirst();
+		if(result.isPresent()) {
+			return result.get() + " in PLZ: " + input;
+		} else {
+			return String.valueOf(-1);
+		}
 	}
 }
